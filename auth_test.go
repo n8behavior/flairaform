8ebc8b8926f195188e8f09db01a24d12bbd1fd2c
n8behavior <mike@thesandmans.com>
2020-01-01 17:12:08 -0500

    refactor auth, stub test

---
+++ auth_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAuthorize(t *testing.T) {
+	t.FailNow()
+}
--- flair/resource_flair_puck.go
+++ flair/resource_flair_puck.go
@@ -1,12 +1,7 @@
 package flair
 
 import (
-	"fmt"
-	"github.com/google/jsonapi"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"net/http"
-	"net/url"
-	"os"
 )
 
 func resourcePuck() *schema.Resource {
@@ -23,43 +18,6 @@ func resourcePuck() *schema.Resource {
 			},
 		},
 	}
-}
-
-type Auth struct {
-	Token   string `json:"access_token"`
-	Expires int    `json:"expires_in"`
-	Type    string `json:"token_type"`
-	Scope   string `json:"scope"`
-}
-
-func authorize() (*Auth, error) {
-	auth := new(Auth)
-	endpoint, err := url.Parse("https://api.flair.co/oauth/token")
-	if err != nil {
-		return auth, fmt.Errorf("Endpoint URL: %s", err)
-	}
-	q := endpoint.Query()
-	q.Set("client_id", os.Getenv("CLIENT_ID"))
-	q.Set("client_secret", os.Getenv("CLIENT_SECRET"))
-	q.Set("scope", "thermostats.view+structures.view+structures.edit")
-	q.Set("grant_type", "client_credentials")
-	endpoint.RawQuery = q.Encode()
-
-	method := "POST"
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, endpoint.String(), nil)
-
-	if err != nil {
-		return auth, fmt.Errorf("Request: %s", err)
-	}
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	res, err := client.Do(req)
-	defer res.Body.Close()
-	jsonapi.UnmarshalPayload(res.Body, auth)
-
-	return auth, nil
 }
 
 func resourcePuckCreate(d *schema.ResourceData, m interface{}) error {
